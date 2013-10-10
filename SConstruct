a1ea35796b950bbc0686f93d608bafb6b8ada84a
Cody Beasley <cody.beasley@knightswarm.com>
2013-10-10 18:46:25 -0500

    Added KS copyright notices to modified files, added handling for build args to prevent building the Ruby, Python, or PHP components (previously only possible to exclude or include all three together)

--- SConstruct
+++ SConstruct
@@ -1,5 +1,21 @@
 # This file has been modified from its orginal sources.
 #
+# (c) 2013 KnightSwarm Handelsbolag
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file has been modified from its orginal sources.
+#
 # (c) 2012 Software in the Public Interest Inc (SPI)
 # (c) 2012 David Pratt
 # 
@@ -65,6 +81,10 @@ build.env.Append(CPPPATH=[
 # debug build flags
 debug = ARGUMENTS.get('debug', 0)
 lite  = ARGUMENTS.get('lite', 0)
+#Testing -IIC
+noruby  = ARGUMENTS.get('no-ruby', 0)
+nopython  = ARGUMENTS.get('no-python', 0)
+nophp  = ARGUMENTS.get('no-php', 0)
 if debug:
     build.env.Append(CPPDEFINES = ('DEBUG', 1))
     if build.is_win32():
--- site_scons/tide.py
+++ site_scons/tide.py
@@ -2,6 +2,22 @@
 
 # This file has been modified from its orginal sources.
 #
+# (c) 2013 KnightSwarm Handelsbolag
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This file has been modified from its orginal sources.
+#
 # Copyright (c) 2012 Software in the Public Interest Inc (SPI)
 # Copyright (c) 2012 David Pratt
 # 
@@ -56,6 +72,9 @@ class BuildConfig(object):
         self.debug = False
         self.os = None
         self.modules = []
+        self.noruby = False
+        self.nopython = False
+        self.nophp = False
         self.tidelite = False
         if not hasattr(os, 'uname') or self.matches('CYGWIN'):
             self.os = 'win32'
